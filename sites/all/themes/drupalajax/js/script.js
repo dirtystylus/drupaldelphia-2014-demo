ed314dfa002292765a4fc7ea1b3c9ca186f1f37f
Mark Llobrera <mark@bluecadet.com>
2014-09-11 21:43:28 -0400

    ajaxinclude script added

--- sites/all/themes/drupalajax/js/script.js
+++ sites/all/themes/drupalajax/js/script.js
@@ -12,4 +12,22 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.load_article = {
+  attach: function(context, settings) {
+    $( "a[data-interaction]" ).once( "nodeajax", function() {
+      $( "a[data-interaction]" ).unbind().bind( "click", function() {
+        $( this ).removeAttr( "data-interaction" ).ajaxInclude();
+        $( this ).on( "ajaxInclude", function () {
+          console.log('done');
+        });
+        return false;
+      });
+      
+    });
+  }
+};
+
+
 })(jQuery, Drupal, this, this.document);
